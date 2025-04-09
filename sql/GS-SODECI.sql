4029a80b411ff6a2502df052da7220f47cce2cc4
KOUADIO-BHEGNIN OTHNIEL KUAWO ANATOLE <othniel.kouadio@gs2e.ci>
2025-04-09 15:54:19 +0000

    date

--- sql/GS-SODECI.sql
+++ sql/GS-SODECI.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `Agent` (
   `adresse` varchar(255),
   `codeStatut` varchar(255),
   `lastUser` varchar(255),
-  `lastDate` datetime(3),
+  `lastDate` date,
   `telephone` varchar(255),
   `nomReseau` varchar(255),
   `codeCategorieAgent` varchar(255),
